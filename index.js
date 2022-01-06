b2b6073aa8bc8a9175c064aa567aad61b7d17d86
WesCorwin <64569639+WestCornfield@users.noreply.github.com>
2022-01-06 17:50:09 +0000

    Adding missing damage to combatant 2

--- index.js
+++ index.js
@@ -16,13 +16,60 @@ const generateCombatants = (msgContent) => {
   return [msgArr[1], msgArr[2]];
 };
 
+const fightText = (msg, combatantOne, combatantTwo) => {
+    msg.reply(combatants[0]+" and "+combatants[1]+" meet in the center of the ring!").then(newMessage => {
+      newMessage.edit("The bell rings!");
+      newMessage.edit(combatants[0]+" hits "+combatants[1]+" with a right hook for 100 damage!")
+      newMessage.edit(combatants[1]+" goes down!")
+      newMessage.edit("The winner is "+combatants[0]);
+    });
+}
+
+const decideTurn = () => {
+  const outcome = Math.floor(Math.random() * 100);
+
+  return (outcome > 50);
+}
+
 const startFight = (msg, combatants) => {
-  msg.reply(combatants[0]+" and "+combatants[1]+" meet in the center of the ring!").then(newMessage => {
-    newMessage.edit("The bell rings!");
-    newMessage.edit(combatants[0]+" hits "+combatants[1]+" with a right hook for 100 damage!")
-    newMessage.edit(combatants[1]+" goes down!")
-    newMessage.edit("The winner is "+combatants[0]);
-  });
+  let combatantOne = {
+    name: combatants[0],
+    hp: 100
+  };
+
+  let combatantTwo = {
+    name: combatants[1],
+    hp: 100
+  };
+
+  let newMessage;
+
+  msg.reply(combatants[0]+" and "+combatants[1]+" meet in the center of the ring!").then(newMsg => {
+    newMessage = newMsg;
+  })
+
+  let playerOnesTurn = decideTurn();
+
+  while (combatantOne.hp > 0 && combatantTwo.hp > 0) {
+    console.log('combatantOne.hp = ' + combatantOne.hp)
+    console.log('combatantTwo.hp = ' + combatantTwo.hp)
+
+    let damage = Math.floor(Math.random() * 30);
+
+    if (playerOnesTurn) {
+      comabatantTwo.health -= damage;
+      newMessage.edit(combatants[0]+" hits "+combatants[1]+" with a right hook for " + damage + " damage!");
+    } else {
+      comabatantOne.health -= damage;
+      newMessage.edit(combatants[0]+" hits "+combatants[1]+" with a right hook for " + damage + " damage!");
+    }
+
+    playerOnesTurn = !playerOnesTurn;
+  }
+
+  const winner = (combatantOne.hp <= 0) ? combatantTwo.name : combatantOne.name
+
+  msg.edit("The winner is " + winner + "!");
 };
 
 client.on("ready", () => {
